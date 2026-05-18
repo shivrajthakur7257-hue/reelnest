e33c70e2c2d6c492424e69b41ca46a0873599801
shivrajthakur7257-hue <shivrajthakur7257@gmail.com>
2026-05-19 00:41:34 +0530

    Updated page.tsx

--- app/instagram/page.tsx
+++ app/instagram/page.tsx
@@ -119,9 +119,20 @@ export default function InstagramPage() {
     setResult(null);
 
     try {
-      const res = await axios.post('https://cobalt-api-1sa1.onrender.com/api/json', {
-  url: url
-});
+     const res = await axios.post(
+  'https://cobalt-api-1sa1.onrender.com/',
+  {
+    url: url,
+    downloadMode: 'auto',
+    filenameStyle: 'classic'
+  },
+  {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }
+);
       console.log('COBALT RESPONSE:', res.data);
     const data = res.data;
 
