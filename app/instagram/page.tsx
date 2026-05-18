e163513d77aefdf49cf640a41f451b3f04c522c1
shivrajthakur7257-hue <shivrajthakur7257@gmail.com>
2026-05-19 00:25:31 +0530

    Updated page.tsx

--- app/instagram/page.tsx
+++ app/instagram/page.tsx
@@ -154,8 +154,13 @@ setResult({
       ));
     } catch (err: any) {
       fetchedRef.current = '';
+console.log('FULL ERROR:', err);
+console.log('ERROR RESPONSE:', err.response?.data);
+
 const msg =
+  err.response?.data?.text ||
   err.response?.data?.error ||
+  err.response?.data?.message ||
   err.message ||
   'Failed to fetch. Make sure the post is public.';
       setError(msg);
