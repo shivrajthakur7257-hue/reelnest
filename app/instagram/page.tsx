25bc60ada84082d6c224741b6063af479fd64b21
shivrajthakur7257-hue <shivrajthakur7257@gmail.com>
2026-05-19 00:12:39 +0530

    Updated page.tsx

--- app/instagram/page.tsx
+++ app/instagram/page.tsx
@@ -120,11 +120,20 @@ export default function InstagramPage() {
 
     try {
       const res = await axios.post('https://cobalt-api-1sa1.onrender.com/', { url });
-      const data = res.data;
+    const data = res.data;
+
+const downloadUrl =
+  data?.url ||
+  data?.picker?.[0]?.url ||
+  data?.picker?.[0]?.audio;
+
+if (!downloadUrl) {
+  throw new Error('Could not retrieve download link');
+}
 
 setResult({
-  thumbnail: data?.picker?.[0]?.thumb || '',
-  title: data?.picker?.[0]?.filename || 'Instagram Media',
+  thumbnail: data?.picker?.[0]?.thumb || data?.thumbnail || '',
+  title: data?.filename || data?.picker?.[0]?.filename || 'Instagram Media',
   author: 'ReelNest',
   isVideo: true,
   downloads: [
@@ -132,7 +141,7 @@ setResult({
       quality: 'HD',
       format: 'mp4',
       size: 'Fast Download',
-      url: data.url,
+      url: downloadUrl,
       direct: true
     }
   ]
