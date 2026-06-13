435525096266ba0cc7e1d697c7fb498b5edf1dd5
shivrajthakur7257-hue <shivrajthakur7257@gmail.com>
2026-06-13 09:27:01 +0530

    Updated package-lock.json

--- components /Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="border-t border-white/10 bg-black px-4 py-8 text-center text-white/50">
-      <p>© 2026 Reelnest Tools. All rights reserved.</p>
-    </footer>
-  );
-}
